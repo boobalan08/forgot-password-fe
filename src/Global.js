5b28d2f648ec775a6a936c7f53d969ffce414e14
boobalan08 <boobalans42@gmail.com>
2023-02-06 10:05:39 +0530

    init

---
+++ src/Global.js
@@ -0,0 +1,3 @@
+// export const API = "http://localhost:4000";
+export const API = "https://forgot-password-backend-dun.vercel.app";
+// export const API = "https://forgot-password-backends.onrender.com";
