b8df56bacd4ef4ba16a743453d0502debde47edb
boobalan08 <boobalans42@gmail.com>
2023-02-06 22:21:36 +0530

    init commit

--- src/Global.js
+++ src/Global.js
@@ -1,3 +1,4 @@
 // export const API = "http://localhost:4000";
-export const API = "https://forgot-password-backend-dun.vercel.app";
 // export const API = "https://forgot-password-backends.onrender.com";
+// export const API = "https://forgotpassword-boobala-backend.vercel.app";
+export const API = "https://forgot-password-node-backend.vercel.app";
